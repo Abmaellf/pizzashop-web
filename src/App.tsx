641f65f94c539aecedd6dc31062226168dc96a62
Abmaellf <abmael.ninha@gmail.com>
2025-06-22 23:39:41 -0400

    feat: add install shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-
+import { Button } from './components/ui/button'
+import './index.css'
 export function App() {
 
   return (
-    <h1> Hello wold</h1>
+    <div className='flex justify-center items-center h-screen'>
+      <Button>  Enviar </Button>
+    </div>
   )
 }
 
