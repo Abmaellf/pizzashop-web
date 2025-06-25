7561c7aee1a148429cb5db35e820954a2d15a34a
Abmaellf <abmael.ninha@gmail.com>
2025-06-25 00:12:47 -0400

    feat: add implement pagelogin

--- src/router.tsx
+++ src/router.tsx
@@ -1,20 +1,20 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
 
-import { AppLayout } from './page/_layouts/app'
-import { AuthLayout } from './page/_layouts/auth'
-import { Dashboard } from './page/app/dashboard'
-import { Signin } from './page/auth/signin'
+import { AppLayout } from "./page/_layouts/app";
+import { AuthLayout } from "./page/_layouts/auth";
+import { Dashboard } from "./page/app/dashboard";
+import { Signin } from "./page/auth/signin";
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<AuthLayout />}>
+      <Route element={<AuthLayout />}>
         <Route path="/" element={<Signin />} />
         <Route path="/signin" element={<Signin />} />
       </Route>
-      <Route path="/" element={<AppLayout />}>
+      <Route element={<AppLayout />}>
         <Route path="/dashboard" element={<Dashboard />} />
       </Route>
     </Routes>
-  )
+  );
 }
