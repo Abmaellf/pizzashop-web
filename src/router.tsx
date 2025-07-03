288fbb00b096c93ba7afc791013e0bbcd535b119
Abmaellf <abmael.ninha@gmail.com>
2025-07-03 15:04:56 -0400

    feat: add 5-Layout do app com cabeçalho

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import { AppLayout } from './page/_layouts/app'
 import { AuthLayout } from './page/_layouts/auth'
 import { Dashboard } from './page/app/dashboard'
+import { Pedidos } from './page/app/pedidos'
 import { SignIn } from './page/auth/sign-in'
 import { SignUp } from './page/auth/sign-up'
 
@@ -10,12 +11,13 @@ export function Router() {
   return (
     <Routes>
       <Route element={<AuthLayout />}>
-        <Route path="/" element={<SignIn />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Route>
       <Route element={<AppLayout />}>
+        <Route path="/" element={<Dashboard />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/orders" element={<Pedidos />} />
       </Route>
     </Routes>
   )
