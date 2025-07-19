f896a8de704ec6b26339eace9c377fb53e4429e4
Abmaellf <abmael.ninha@gmail.com>
2025-07-19 16:43:26 -0400

    feat: add 2.15-Página 404 (Not Found)

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 
 import { AppLayout } from './page/_layouts/app'
 import { AuthLayout } from './page/_layouts/auth'
+import { NotFound } from './page/404'
 import { Dashboard } from './page/app/dashboard/dashboard'
 import { Orders } from './page/app/orders/orders'
 import { SignIn } from './page/auth/sign-in'
@@ -18,6 +19,7 @@ export function Router() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/orders" element={<Orders />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   )
