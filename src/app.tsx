48cb53ff2a8699fd87fa34305fa380ddec06e5cd
Abmaellf <abmael.ninha@gmail.com>
2025-07-23 11:04:41 -0400

    feat: add axios and queryClient

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import './index.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 // import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from './components/theme/theme-provider'
+import { queryClient } from './lib/react-query'
 import { Router } from './router'
 
 export function App() {
@@ -14,7 +16,9 @@ export function App() {
       {/* <Helmet titleTemplate="%s | Pizza.Shop" /> */}
       <ThemeProvider defaultTheme="dark" storageKey="pizza-shop-theme">
         <Toaster richColors />
-        <Router />
+        <QueryClientProvider client={queryClient}>
+          <Router />
+        </QueryClientProvider>
         {/* </HelmetProvider> */}
       </ThemeProvider>
     </BrowserRouter>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import { App } from './App'
+import { App } from "./app";
 
-createRoot(document.getElementById('root')!).render(
+// eslint-disable-next-line prettier/prettier
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
