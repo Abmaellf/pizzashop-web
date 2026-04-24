67b433803e9f76bbe2bd217c3ef560fba4569b64
Abmaellf <abmael.ninha@gmail.com>
2026-04-24 08:30:46 -0400

    feat: pagina de error boundary e error page

--- src/router.tsx
+++ src/router.tsx
@@ -4,23 +4,26 @@ import { AppLayout } from './page/_layouts/app'
 import { AuthLayout } from './page/_layouts/auth'
 import { NotFound } from './page/404'
 import { Dashboard } from './page/app/dashboard/dashboard'
+import { ErrorBoundary } from './page/app/error/error-boundary'
 import { Orders } from './page/app/orders/orders'
 import { SignIn } from './page/auth/sign-in'
 import { SignUp } from './page/auth/sign-up'
 
 export function Router() {
   return (
-    <Routes>
-      <Route element={<AuthLayout />}>
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Route>
-      <Route element={<AppLayout />}>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+    <ErrorBoundary>
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </ErrorBoundary>
   )
 }
